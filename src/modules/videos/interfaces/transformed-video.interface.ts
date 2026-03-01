ae48ceb5a497c59ff34164ae00e4c55ca8a57477
Stiven-Chacon <stivenchacon031@gmail.com>
2026-03-01 17:01:06 -0500

    feat(videos): add transformed video interface

---
+++ src/modules/videos/interfaces/transformed-video.interface.ts
@@ -0,0 +1,11 @@
+export interface TransformedVideo {
+  id: string;
+  title: string;
+  channelTitle: string;
+  thumbnailUrl: string;
+  views: number;
+  likes: number;
+  commentsEnabled: boolean;
+  hypeLevel: number;
+  publishedAt: string;
+}
